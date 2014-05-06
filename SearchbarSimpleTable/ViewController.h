a14a73b6789f97d2efb637da282adcb141f85796
Leslie Han <lesliehan9210@gmail.com>
2014-05-06 17:34:43 +0800

    Initial Commit

---
+++ SearchbarSimpleTable/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SearchbarSimpleTable
+//
+//  Created by Taagoo'iMac on 14-5-6.
+//  Copyright (c) 2014年 Taagoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SearchbarSimpleTable/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SearchbarSimpleTable
+//
+//  Created by Taagoo'iMac on 14-5-6.
+//  Copyright (c) 2014年 Taagoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
